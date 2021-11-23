b4c048071695ea41cfcb5b04b722e3b6ee3e4243
sofiaversoza <sofversoza@gmail.com>
2021-11-22 23:27:43 -0700

    commit tree df4d95ce1575cabbfc1af239065699fe57a8fdc2

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const scuberHq = 42;
+
+function distanceFromHqInBlocks(location) {
+    let distanceB = location - scuberHq;
+    return Math.abs(distanceB);
+}
+
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let distanceT = (start - destination) * 264;
+    return Math.abs(distanceT);
+}
+
+function calculatesFarePrice(start, destination) {
+    let totalFt = distanceTravelledInFeet(start, destination);
+
+    if (totalFt < 400)
+        return 0;
+    else if (totalFt < 2000) 
+        return 2.56;
+    else if (totalFt > 2000 && totalFt < 2500)  
+        return 25;
+    else 
+        return 'cannot travel that far';    
+}
